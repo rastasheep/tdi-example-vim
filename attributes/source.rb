443a9189eb235e58f5f56ee64be3b52ed936f4c2
Aleksandar Diklic <rastasheep3@gmail.com>
2015-02-26 10:57:22 +0100

    Fix installation from source

--- attributes/source.rb
+++ attributes/source.rb
@@ -8,12 +8,12 @@ default['vim']['extra_packages'] = []
 default['vim']['install_method'] = 'package'
 
 default['vim']['source']['version']       = '7.4'
-default['vim']['source']['checksum']      = '607e135c559be642f210094ad023dc65'
+default['vim']['source']['checksum']      = 'd0f5a6d2c439f02d97fa21bd9121f4c5abb1f6cd8b5a79d3ca82867495734ade'
 default['vim']['source']['prefix']        = "/usr/local"
 default['vim']['source']['configuration'] = "--without-x --enable-pythoninterp --enable-rubyinterp --enable-cscope  --with-features=huge --prefix=#{default['vim']['source']['prefix']}"
 
 if platform_family? "rhel"
   default['vim']['source']['dependencies'] = %w{ python-devel ncurses ncurses-devel ruby ruby-devel perl-devel ctags gcc make }
 else
-  default['vim']['source']['dependencies'] = %w{ python-dev libncurses5-dev ruby ruby-dev libperl-dev ctags gcc make }
+  default['vim']['source']['dependencies'] = %w{ python-dev libncurses5-dev ruby ruby-dev libperl-dev exuberant-ctags gcc make }
 end
